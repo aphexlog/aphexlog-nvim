26ea7c4013763dab822e695e4dca189e6e491dbb
Aaron West <aphexlog@gmail.com>
2024-10-26 17:09:57 -0500

    branding nvim setup

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -1,16 +1,55 @@
 return {
   "goolord/alpha-nvim",
-  -- dependencies = { 'echasnovski/mini.icons' },
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
-    local startify = require("alpha.themes.startify")
-    -- available: devicons, mini, default is mini
-    -- if provider not loaded and enabled is true, it will try to use another provider
-    -- add some ui options
-    startify.section.header.val = {
+    -- Load alpha with error handling
+    local status_ok, alpha = pcall(require, "alpha")
+    if not status_ok then
+      return
+    end
+
+    -- Setup dashboard theme
+    local dashboard = require("alpha.themes.dashboard")
+
+    -- Custom header with ASCII art and a welcome message
+    dashboard.section.header.val = {
       " Welcome back to the matrix, Aphexlog",
+      [[          ▀████▀▄▄              ▄█ ]],
+      [[            █▀    ▀▀▄▄▄▄▄    ▄▄▀▀█ ]],
+      [[    ▄        █          ▀▀▀▀▄  ▄▀  ]],
+      [[   ▄▀ ▀▄      ▀▄              ▀▄▀  ]],
+      [[  ▄▀    █     █▀   ▄█▀▄      ▄█    ]],
+      [[  ▀▄     ▀▄  █     ▀██▀     ██▄█   ]],
+      [[   ▀▄    ▄▀ █   ▄██▄   ▄  ▄  ▀▀ █  ]],
+      [[    █  ▄▀  █    ▀██▀    ▀▀ ▀▀  ▄▀  ]],
+      [[   █   █  █      ▄▄           ▄▀   ]],
     }
-    startify.file_icons.provider = "devicons"
-    require("alpha").setup(startify.config)
+
+    -- Button configuration
+    dashboard.section.buttons.val = {
+      dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
+      dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+      dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
+      dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
+      dashboard.button("c", "  Configuration", ":e ~/.config/nvim/init.vim<CR>"),
+      dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
+    }
+
+    -- Footer setup with a custom message
+    local function footer()
+      return "Don't Stop Until You are Proud..."
+    end
+    dashboard.section.footer.val = footer()
+
+    -- Setting highlight groups
+    dashboard.section.header.opts.hl = "Include"
+    dashboard.section.buttons.opts.hl = "Keyword"
+    dashboard.section.footer.opts.hl = "Type"
+
+    -- Additional options and setup
+    dashboard.opts.opts.noautocmd = true
+
+    -- Initialize alpha with the configured dashboard
+    alpha.setup(dashboard.opts)
   end,
 }
