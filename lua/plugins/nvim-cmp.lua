c786a8e233bfb42473c60bcba179e9a0be2d8530
Aaron West <aphexlog@gmail.com>
2024-12-03 15:45:18 -0600

    refactor: Adjust nvim-cmp sorting configuration with priority weight

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -3,8 +3,8 @@ return {
   ---@param opts cmp.ConfigSchema
   opts = function(_, opts)
     local cmp = require("cmp")
-    
     opts.sorting = {
+      priority_weight = 2,
       comparators = {
         cmp.config.compare.offset,
         cmp.config.compare.exact,
@@ -13,9 +13,9 @@ return {
         cmp.config.compare.sort_text,
         cmp.config.compare.length,
         cmp.config.compare.order,
-      }
+      },
     }
-    
+
     return opts
   end,
 }
