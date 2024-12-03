4f46b97dbeca8b80b26f8c4fa50efff8a5ccd625
Aaron West <aphexlog@gmail.com>
2024-12-03 16:00:04 -0600

    refactor: Modify nvim-cmp sorting to prioritize variables and simplify mappings

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -3,37 +3,27 @@ return {
   ---@param opts cmp.ConfigSchema
   opts = function(_, opts)
     local cmp = require("cmp")
-    
-    -- Add mappings
-    opts.mapping = vim.tbl_extend("force", opts.mapping or {}, {
-      ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-      ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-      -- Jump to next/previous variable
-      ["<C-v>"] = cmp.mapping(function(fallback)
-        local entries = cmp.get_entries()
-        for i, entry in ipairs(entries) do
-          if entry.kind == cmp.lsp.CompletionItemKind.Variable then
-            cmp.select_next_item({ behavior = cmp.SelectBehavior.Select, count = i })
-            return
-          end
-        end
-        fallback()
-      end),
-      ["<C-S-v>"] = cmp.mapping(function(fallback)
-        local entries = cmp.get_entries()
-        for i = #entries, 1, -1 do
-          if entries[i].kind == cmp.lsp.CompletionItemKind.Variable then
-            cmp.select_prev_item({ behavior = cmp.SelectBehavior.Select, count = #entries - i + 1 })
-            return
-          end
-        end
-        fallback()
-      end),
-    })
-    
     opts.sorting = {
-      priority_weight = 2,
+      priority_weight = 1,
       comparators = {
+        function(entry1, entry2)
+          -- Give higher priority to variables
+          local kind_priority = {
+            Variable = 1, -- Assign highest priority to variables
+            Function = 2,
+            Method = 3,
+            Field = 4,
+            Class = 5,
+            Interface = 6,
+            Module = 7,
+          }
+
+          local kind1 = kind_priority[entry1:get_kind()] or 100
+          local kind2 = kind_priority[entry2:get_kind()] or 100
+          if kind1 ~= kind2 then
+            return kind1 < kind2
+          end
+        end,
         cmp.config.compare.offset,
         cmp.config.compare.exact,
         cmp.config.compare.score,
