eab3b57ab50ae950f8ac55484e6b583faee63b1b
Aaron West <aphexlog@gmail.com>
2025-02-09 13:06:48 -0600

    refactor: Simplify wezterm config_builder initialization

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,12 +1,6 @@
 local wezterm = require("wezterm")
 
 -- This will hold the configuration.
-local config = {}
-
--- In newer versions of wezterm, use the config_builder which will
--- help provide clearer error messages
-if wezterm.config_builder then
-  config = wezterm.config_builder()
-end
+local config = wezterm.config_builder()
 
 return config
