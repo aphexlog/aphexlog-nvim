59c9553ffe0a1b5a09afb1aa59c4a631bb224756
Aaron West <aphexlog@gmail.com>
2025-02-09 13:06:37 -0600

    feat: Add initial WezTerm configuration with API setup

---
+++ .wezterm.lua
@@ -0,0 +1,11 @@
+local wezterm = require("wezterm")
+
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+return config
