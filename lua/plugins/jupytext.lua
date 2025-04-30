ccfee16c2b5db5dbd9a600f0352e8d0fb7e15419
Aaron West <aphexlog@gmail.com>
2025-04-30 14:11:18 -0500

    switching back to zsh for compatibility

--- lua/plugins/jupytext.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "GCBallesteros/jupytext.nvim",
-  config = true,
-  -- Depending on your nvim distro or config you may need to make the loading not lazy
-  lazy = true,
-  ft = { "python" },
-}
